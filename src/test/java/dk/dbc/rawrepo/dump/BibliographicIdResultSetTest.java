2af75c55e10719972d430c8b06dfa19e42f6d7fc
Anders Thrane Michelsen <atm@dbc.dk>
2019-03-05 13:17:49 +0100

    Add unit tests

--- src/test/java/dk/dbc/rawrepo/dump/BibliographicIdResultSetTest.java
+++ src/test/java/dk/dbc/rawrepo/dump/BibliographicIdResultSetTest.java
@@ -1,77 +1,249 @@
 package dk.dbc.rawrepo.dump;
 
+import dk.dbc.rawrepo.dao.HoldingsItemsBean;
+import dk.dbc.rawrepo.dao.RawRepoBean;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
 public class BibliographicIdResultSetTest {
 
-//    @Mock
-//    private RawRepoBean bean;
-//
-//    @Before
-//    public void setUp() {
-//        MockitoAnnotations.initMocks(this);
-//    }
-//
-//    @Test
-//    public void testNormalList() throws Exception {
-//        Params params = new Params();
-//        params.setAgencies(Arrays.asList(870970));
-//        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(bean, 2, params);
-//        List<String> res = Arrays.asList("A", "B", "C", "D", "E");
-//
-//        when(bean.getBibliographicRecordIdsForEnrichmentAgency(eq(870970), eq(191919))).thenReturn(res);
-//
-//        resultSet.fetchRecordIds(870970, AgencyType.DBC);
-//        Assert.assertThat(resultSet.size(), is(5));
-//        Assert.assertThat(resultSet.next(), is(Arrays.asList("A", "B")));
-//        Assert.assertThat(resultSet.next(), is(Arrays.asList("C", "D")));
-//        Assert.assertThat(resultSet.next(), is(Arrays.asList("E")));
-//        Assert.assertThat(resultSet.next(), is(nullValue()));
-//    }
-//
-//
-//    @Test
-//    public void testListSmallerThanSliceSize() throws Exception {
-//        Params params = new Params();
-//        params.setAgencies(Arrays.asList(870970));
-//
-//        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(bean, 2, params);
-//        List<String> res = Arrays.asList("A");
-//
-//        when(bean.getBibliographicRecordIdsForEnrichmentAgency(eq(870970), eq(191919))).thenReturn(res);
-//
-//        resultSet.fetchRecordIds(870970, AgencyType.DBC);
-//        Assert.assertThat(resultSet.size(), is(1));
-//        Assert.assertThat(resultSet.next(), is(Arrays.asList("A")));
-//        Assert.assertThat(resultSet.next(), is(nullValue()));
-//    }
-//
-//    @Test
-//    public void testListSizeEqualsSliceSize() throws Exception {
-//        Params params = new Params();
-//        params.setAgencies(Arrays.asList(870970));
-//
-//        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(bean, 2, params);
-//        List<String> res = Arrays.asList("A", "B");
-//
-//        when(bean.getBibliographicRecordIdsForEnrichmentAgency(eq(870970), eq(191919))).thenReturn(res);
-//
-//        resultSet.fetchRecordIds(870970, AgencyType.DBC);
-//        Assert.assertThat(resultSet.size(), is(2));
-//        Assert.assertThat(resultSet.next(), is(Arrays.asList("A", "B")));
-//        Assert.assertThat(resultSet.next(), is(nullValue()));
-//    }
-//
-//    @Test
-//    public void testEmptyList() throws Exception {
-//        Params params = new Params();
-//        params.setAgencies(Arrays.asList(870970));
-//
-//        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(bean, 2, params);
-//        List<String> res = Arrays.asList();
-//
-//        when(bean.getBibliographicRecordIdsForEnrichmentAgency(eq(870970), eq(191919))).thenReturn(res);
-//
-//        resultSet.fetchRecordIds(870970, AgencyType.DBC);
-//        Assert.assertThat(resultSet.size(), is(0));
-//        Assert.assertThat(resultSet.next(), is(nullValue()));
-//    }
+    private final HashMap<String, String> rawrepoRecordIdsFor710100 = new HashMap<String, String>() {{
+        put("A", "text/marcxchange");
+        put("B", "text/marcxchange");
+        put("C", "text/enrichment+marcxchange");
+        put("D", "text/enrichment+marcxchange");
+    }};
+
+    private final HashMap<String, String> holdingsRecordIdsFor710100 = new HashMap<String, String>() {{
+        put("B", "holdings");
+        put("D", "holdings");
+        put("E", "holdings");
+    }};
+
+    private final HashMap<String, String> rawrepoRecordIdsFor870970 = new HashMap<String, String>() {{
+        put("A", "text/marcxchange");
+        put("B", "text/marcxchange");
+        put("C", "text/marcxchange");
+        put("D", "text/marcxchange");
+        put("E", "text/marcxchange");
+    }};
+
+    @Mock
+    private RawRepoBean rawRepoBean;
+
+    @Mock
+    private HoldingsItemsBean holdingsItemsBean;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testNormalList() throws Exception {
+        Params params = new Params();
+        params.setAgencies(Arrays.asList(870970));
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(870970), eq(RecordStatus.ALL))).thenReturn(rawrepoRecordIdsFor870970);
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(870970, AgencyType.DBC, params, 2, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(5));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("A", "text/marcxchange");
+            put("B", "text/marcxchange");
+        }}));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("C", "text/marcxchange");
+            put("D", "text/marcxchange");
+        }}));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("E", "text/marcxchange");
+        }}));
+    }
+
+    @Test
+    public void testListSmallerThanSliceSize() throws Exception {
+        Params params = new Params();
+        params.setAgencies(Arrays.asList(870970));
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(870970), eq(RecordStatus.ALL))).thenReturn(rawrepoRecordIdsFor870970);
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(870970, AgencyType.DBC, params, 10, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(5));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("A", "text/marcxchange");
+            put("B", "text/marcxchange");
+            put("C", "text/marcxchange");
+            put("D", "text/marcxchange");
+            put("E", "text/marcxchange");
+        }}));
+    }
+
+    @Test
+    public void testListSizeEqualsSliceSize() throws Exception {
+        Params params = new Params();
+        params.setAgencies(Arrays.asList(870970));
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(870970), eq(RecordStatus.ALL))).thenReturn(rawrepoRecordIdsFor870970);
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(870970, AgencyType.DBC, params, 5, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(5));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("A", "text/marcxchange");
+            put("B", "text/marcxchange");
+            put("C", "text/marcxchange");
+            put("D", "text/marcxchange");
+            put("E", "text/marcxchange");
+        }}));
+    }
+
+    @Test
+    public void testEmptyList() throws Exception {
+        Params params = new Params();
+        params.setAgencies(Arrays.asList(870970));
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(870970), eq(RecordStatus.ALL))).thenReturn(new HashMap<>());
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(870970, AgencyType.DBC, params, 2, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(0));
+    }
+
+    @Test
+    public void testFBSAllRecordTypes() throws Exception {
+        Params params = new Params();
+        params.setRecordType(new ArrayList<>());
+        params.getRecordType().add(RecordType.LOCAL.toString());
+        params.getRecordType().add(RecordType.ENRICHMENT.toString());
+        params.getRecordType().add(RecordType.HOLDINGS.toString());
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(710100), eq(RecordStatus.ALL))).thenReturn(rawrepoRecordIdsFor710100);
+        when(holdingsItemsBean.getRecordIdsWithHolding(eq(710100))).thenReturn(holdingsRecordIdsFor710100);
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(710100, AgencyType.FBS, params, 2, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(5));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("A", "text/marcxchange");
+            put("B", "text/marcxchange");
+        }}));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("C", "text/enrichment+marcxchange");
+            put("D", "text/enrichment+marcxchange");
+        }}));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("E", "holdings");
+        }}));
+    }
+
+    @Test
+    public void testFBSLocalAndEnrichments() throws Exception {
+        Params params = new Params();
+        params.setRecordType(new ArrayList<>());
+        params.getRecordType().add(RecordType.LOCAL.toString());
+        params.getRecordType().add(RecordType.ENRICHMENT.toString());
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(710100), eq(RecordStatus.ALL))).thenReturn(rawrepoRecordIdsFor710100);
+        when(holdingsItemsBean.getRecordIdsWithHolding(eq(710100))).thenReturn(holdingsRecordIdsFor710100);
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(710100, AgencyType.FBS, params, 2, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(4));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("A", "text/marcxchange");
+            put("B", "text/marcxchange");
+        }}));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("C", "text/enrichment+marcxchange");
+            put("D", "text/enrichment+marcxchange");
+        }}));
+    }
+
+    @Test
+    public void testFSBLocalAndHoldings() throws Exception {
+        Params params = new Params();
+        params.setRecordType(new ArrayList<>());
+        params.getRecordType().add(RecordType.LOCAL.toString());
+        params.getRecordType().add(RecordType.HOLDINGS.toString());
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(710100), eq(RecordStatus.ALL))).thenReturn(rawrepoRecordIdsFor710100);
+        when(holdingsItemsBean.getRecordIdsWithHolding(eq(710100))).thenReturn(holdingsRecordIdsFor710100);
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(710100, AgencyType.FBS, params, 2, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(4));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("A", "text/marcxchange");
+            put("B", "text/marcxchange");
+        }}));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("D", "text/enrichment+marcxchange");
+            put("E", "holdings");
+        }}));
+    }
+
+    @Test
+    public void testFSBEnrichmentAndHoldings() throws Exception {
+        Params params = new Params();
+        params.setRecordType(new ArrayList<>());
+        params.getRecordType().add(RecordType.ENRICHMENT.toString());
+        params.getRecordType().add(RecordType.HOLDINGS.toString());
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(710100), eq(RecordStatus.ALL))).thenReturn(rawrepoRecordIdsFor710100);
+        when(holdingsItemsBean.getRecordIdsWithHolding(eq(710100))).thenReturn(holdingsRecordIdsFor710100);
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(710100, AgencyType.FBS, params, 2, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(4));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("B", "text/marcxchange");
+            put("C", "text/enrichment+marcxchange");
+        }}));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("D", "text/enrichment+marcxchange");
+            put("E", "holdings");
+        }}));
+    }
+
+    @Test
+    public void testFSBHoldingsOnly() throws Exception {
+        Params params = new Params();
+        params.setRecordType(new ArrayList<>());
+        params.getRecordType().add(RecordType.HOLDINGS.toString());
+        params.setRecordStatus(RecordStatus.ALL.toString());
+
+        when(rawRepoBean.getBibliographicRecordIdForAgency(eq(710100), eq(RecordStatus.ALL))).thenReturn(rawrepoRecordIdsFor710100);
+        when(holdingsItemsBean.getRecordIdsWithHolding(eq(710100))).thenReturn(holdingsRecordIdsFor710100);
+
+        BibliographicIdResultSet resultSet = new BibliographicIdResultSet(710100, AgencyType.FBS, params, 3, rawRepoBean, holdingsItemsBean);
+
+        Assert.assertThat(resultSet.size(), is(3));
+        Assert.assertThat(resultSet.next(), is(new HashMap<String, String>() {{
+            put("B", "text/marcxchange");
+            put("D", "text/enrichment+marcxchange");
+            put("E", "holdings");
+        }}));
+    }
 }
