28e86444987c7561bfeaee6c4870af3228acd8ee
Anders Thrane Michelsen <atm@dbc.dk>
2019-07-30 13:55:20 +0200

    Clean up code

--- src/main/java/dk/dbc/rawrepo/dump/DumpService.java
+++ src/main/java/dk/dbc/rawrepo/dump/DumpService.java
@@ -77,9 +77,9 @@ public class DumpService {
     @Produces({MediaType.TEXT_PLAIN})
     public Response dumpLibraryRecordsDryRun(AgencyParams params) {
         try {
-            List<ParamsValidationItem> paramsValidationItemList = params.validate(openAgency.getService());
+            final List<ParamsValidationItem> paramsValidationItemList = params.validate(openAgency.getService());
             if (paramsValidationItemList.size() > 0) {
-                ParamsValidation paramsValidation = new ParamsValidation();
+                final ParamsValidation paramsValidation = new ParamsValidation();
                 paramsValidation.setErrors(paramsValidationItemList);
                 LOGGER.info("Validation errors: {}", paramsValidation);
                 return Response.status(400).entity(jsonbContext.marshall(paramsValidation)).build();
@@ -95,11 +95,11 @@ public class DumpService {
                 public void write(OutputStream out) throws WebApplicationException {
                     try {
                         for (Integer agencyId : params.getAgencies()) {
-                            AgencyType agencyType = AgencyType.getAgencyType(openAgency.getService(), agencyId);
-                            HashMap<String, String> record = getRecords(agencyId, params);
-                            HashMap<String, String> holdings = getHoldings(agencyId, agencyType, params, true);
+                            final AgencyType agencyType = AgencyType.getAgencyType(openAgency.getService(), agencyId);
+                            final HashMap<String, String> record = getRecords(agencyId, params);
+                            final HashMap<String, String> holdings = getHoldings(agencyId, agencyType, params, true);
 
-                            BibliographicIdResultSet bibliographicIdResultSet = new
+                            final BibliographicIdResultSet bibliographicIdResultSet = new
                                     BibliographicIdResultSet(params, agencyType, SLICE_SIZE, record, holdings);
 
                             out.write(String.format("%s: %s%n", agencyId, bibliographicIdResultSet.size()).getBytes());
@@ -130,9 +130,9 @@ public class DumpService {
         // The service is meant to be called from curl, so the error message should be easy to read.
         // Therefor the message is simple text instead of JSON or HTML
         try {
-            List<ParamsValidationItem> paramsValidationItemList = params.validate(openAgency.getService());
+            final List<ParamsValidationItem> paramsValidationItemList = params.validate(openAgency.getService());
             if (paramsValidationItemList.size() > 0) {
-                ParamsValidation paramsValidation = new ParamsValidation();
+                final ParamsValidation paramsValidation = new ParamsValidation();
                 paramsValidation.setErrors(paramsValidationItemList);
                 LOGGER.info("Validation errors: {}", paramsValidation);
                 return Response.status(400).entity(jsonbContext.marshall(paramsValidation)).build();
@@ -150,14 +150,14 @@ public class DumpService {
                 public void write(OutputStream out) throws WebApplicationException {
                     try {
                         for (Integer agencyId : params.getAgencies()) {
-                            RecordByteWriter recordByteWriter = new RecordByteWriter(out, params);
+                            final RecordByteWriter recordByteWriter = new RecordByteWriter(out, params);
                             recordByteWriter.writeHeader();
-                            AgencyType agencyType = AgencyType.getAgencyType(openAgency.getService(), agencyId);
-                            HashMap<String, String> record = getRecords(agencyId, params);
-                            HashMap<String, String> holdings = getHoldings(agencyId, agencyType, params, false);
+                            final AgencyType agencyType = AgencyType.getAgencyType(openAgency.getService(), agencyId);
+                            final HashMap<String, String> record = getRecords(agencyId, params);
+                            final HashMap<String, String> holdings = getHoldings(agencyId, agencyType, params, false);
 
                             LOGGER.info("Opening connection and RecordResultSet...");
-                            BibliographicIdResultSet bibliographicIdResultSet = new
+                            final BibliographicIdResultSet bibliographicIdResultSet = new
                                     BibliographicIdResultSet(params, agencyType, SLICE_SIZE, record, holdings);
 
                             LOGGER.info("Found {} records", bibliographicIdResultSet.size());
@@ -178,7 +178,7 @@ public class DumpService {
 
                                 // Execute the threads when either the outstanding thread count has reached max or it is the last loop
                                 if (loopCount % MAX_THREAD_COUNT == 0 || !bibliographicIdResultSet.hasNext()) {
-                                    List<Future<Boolean>> futures = executor.invokeAll(threadList);
+                                    final List<Future<Boolean>> futures = executor.invokeAll(threadList);
                                     for (Future f : futures) {
                                         try {
                                             f.get(); // We don't care about the result, we just want to see if there was an exception during execution
@@ -215,30 +215,30 @@ public class DumpService {
     @Consumes({MediaType.TEXT_PLAIN})
     @Produces({MediaType.TEXT_PLAIN})
     public Response dumpSingleRecords(String input,
-                                      @DefaultValue("false") @QueryParam("output-encoding") String outputEncoding,
-                                      @DefaultValue("false") @QueryParam("output-format") String outputFormat) {
-        RecordParams params = new RecordParams();
+                                      @DefaultValue("UTF-8") @QueryParam("output-encoding") String outputEncoding,
+                                      @DefaultValue("LINE") @QueryParam("output-format") String outputFormat) {
+        LOGGER.info(input);
+        final RecordParams params = new RecordParams();
         params.setOutputEncoding(outputEncoding);
         params.setOutputFormat(outputFormat);
         // The service is meant to be called from curl, so the error message should be easy to read.
         // Therefor the message is simple text instead of JSON or HTML
         try {
-            List<RecordIdDTO> recordIdDTOs = new ArrayList<>();
-            BufferedReader bufReader = new BufferedReader(new StringReader(input));
-            String line=null;
-            while( (line=bufReader.readLine()) != null )
-            {
+            final List<RecordIdDTO> recordIdDTOs = new ArrayList<>();
+            final BufferedReader bufReader = new BufferedReader(new StringReader(input));
+            String line;
+            while ((line = bufReader.readLine()) != null) {
                 String[] vars = line.split(":");
                 recordIdDTOs.add(new RecordIdDTO(vars[0], Integer.parseInt(vars[1])));
             }
 
             params.setRecordIds(recordIdDTOs);
 
-            List<ParamsValidationItem> paramsValidationItemList = params.validate();
+            final List<ParamsValidationItem> paramsValidationItemList = params.validate();
             LOGGER.info("Dump single dumping {} records", params.getRecordIds().size());
             LOGGER.info(params.toString());
             if (paramsValidationItemList.size() > 0) {
-                ParamsValidation paramsValidation = new ParamsValidation();
+                final ParamsValidation paramsValidation = new ParamsValidation();
                 paramsValidation.setErrors(paramsValidationItemList);
                 LOGGER.info("Validation errors: {}", paramsValidation);
                 return Response.status(400).entity(jsonbContext.marshall(paramsValidation)).build();
@@ -256,13 +256,13 @@ public class DumpService {
                 public void write(OutputStream out) throws WebApplicationException {
                     try {
                         for (Integer agencyId : params.getAgencies()) {
-                            RecordByteWriter recordByteWriter = new RecordByteWriter(out, params);
+                            final RecordByteWriter recordByteWriter = new RecordByteWriter(out, params);
                             recordByteWriter.writeHeader();
-                            AgencyType agencyType = AgencyType.getAgencyType(openAgency.getService(), agencyId);
-                            HashMap<String, String> record = getRecords(agencyId, params);
+                            final AgencyType agencyType = AgencyType.getAgencyType(openAgency.getService(), agencyId);
+                            final HashMap<String, String> record = getRecords(agencyId, params);
 
                             LOGGER.info("Opening connection and RecordResultSet...");
-                            BibliographicIdResultSet bibliographicIdResultSet = new
+                            final BibliographicIdResultSet bibliographicIdResultSet = new
                                     BibliographicIdResultSet(SLICE_SIZE, record);
 
                             LOGGER.info("Found {} records", bibliographicIdResultSet.size());
@@ -283,7 +283,7 @@ public class DumpService {
 
                                 // Execute the threads when either the outstanding thread count has reached max or it is the last loop
                                 if (loopCount % MAX_THREAD_COUNT == 0 || !bibliographicIdResultSet.hasNext()) {
-                                    List<Future<Boolean>> futures = executor.invokeAll(threadList);
+                                    final List<Future<Boolean>> futures = executor.invokeAll(threadList);
                                     for (Future f : futures) {
                                         try {
                                             f.get(); // We don't care about the result, we just want to see if there was an exception during execution
