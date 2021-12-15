9700438f1ef2296ba99d11949872f92ce6c27e78
Anders Thrane Michelsen <atm@dbc.dk>
2021-12-15 09:34:17 +0100

    Remove license headers
    Auditors: akri, jbr, mib, hwha

--- src/main/java/dk/dbc/rawrepo/output/OutputStreamMarcXchangeRecordWriter.java
+++ src/main/java/dk/dbc/rawrepo/output/OutputStreamMarcXchangeRecordWriter.java
@@ -1,8 +1,3 @@
-/*
- * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
- *  See license text at https://opensource.dbc.dk/licenses/gpl-3.0
- */
-
 package dk.dbc.rawrepo.output;
 
 import dk.dbc.marc.binding.MarcRecord;
